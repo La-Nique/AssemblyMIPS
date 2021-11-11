d783d6036ce880820b2d6657cefcc6673bd0bacc
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-11-11 00:13:37 -0500

    updated.

--- smartStudent.asm
+++ smartStudent.asm
@@ -19,9 +19,26 @@
 #     object smartOne is located at the address 0x0000 0F00
 #     object smartOne has no "padding" and aliasing in it. The data members are stored in the order in which they are declared (mID, then mAge, then mCreditsEarned).
 
-# my thought process:
-# place 0x0000 0F00 into a register
-# load s2 with address 4 bytes away
-# modify value
+addi $s6, $zero, 0xF000
+# s6 == 0xF000 ; smartOne's object address.
+lhu $s2, 4($s6)
+# s2 == unsigned short int smartOne's mAge address.
+addi $s2, $s2, 1
+# s2 = smartOne.mAge++;
+sh $s2, 4($s6)
+# store modified value into address.
+lh $s4, 6($s6)
+# s4 == "regular" short int smartOne's mCreditsEarned address.
+addi $s4, $s4, 3
+# s3+=3 ; mCreditsEarned += 3;
+sh $s4, 6($s6)
+# store modified value into address.
 
-addi $s6, $zero, 0xF00
+# my thought process:
+# place 0x 0000 0F00 into a register, s6
+# load a register, s2, with address in s6, offset by 4 bytes to access mAge and bypass mID
+# increment mAge by 1 ; smartOne.mAge++;
+# store modified value into smartOne.mAge's address
+# load a register, s4, with address in s6, offset by 6 bytes to access smartOne.mCreditsEarned and bypass mID && mAge
+# add three to value in address ; smartOne.mCreditsEarned = smartOne.mCreditsEarned + 3; == smartOne.mCreditsEarned+=3;
+# store modified value into smartOne.mCreditsEarned's address
