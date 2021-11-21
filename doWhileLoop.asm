2c9974d18f5fc538e1205647bb46f0df76aa797c
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-11-21 16:52:05 -0500

    revised.

--- doWhileLoop.asm
+++ doWhileLoop.asm
@@ -1,20 +1,20 @@
 # do
 # {
-#   var-=20; // var unsigned int in address s3.
+#   var- = 20; // var unsigned int in address s3.
 # }
-# while(var>100);
+# while(var > 100);
 
 lw $t0, 0($s3)
 # load var value in t0
+addi $t7, $zero, 100
+# t7 = 100
 
-START:
+LOOP:
   addi $t0, $t0, -20
   # var- = 20
-  addi $t7, $zero, 100
-  # t7 = 100
   sltu $t5, $t7, $t0
-  # is 100 < var? 0 | 1?
-  bne $t5, $zero, START
-  # if t5 != false --> go to START
+  # is 100 < var? 0 (false) | 1 (true)?
+  bne $t5, $zero, LOOP
+  # if t5 != 0 --> go to LOOP
   sw $t0, 0($s3)
   #store modified value back in var
