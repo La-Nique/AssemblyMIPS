ea824323b5aa627ba6504274895f827eb862b9a5
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-12-12 01:39:38 -0500

    replaced code

--- setBits.asm
+++ setBits.asm
@@ -1,14 +1,28 @@
-# write function ADD that takes three numbers from a0, a1, a2 and returns their sum in v0. 
-# You can modify only s-registers inside the function.
+# setting, clearing, inverting 
+#    // set = 1, cleared = 0
 
-ADD:
-  addi $sp $sp, -4
-  sw $s0, 0($sp)
+# Instructions: s0 holds value. Go to L1 if bit number 2 is set. 
+#   // bit is set means if bit is equal to 1. 
 
-  add $s0, $a0, $a1
-  add $v0, $s0, $a2
-  
-  lw $s0, 0($sp)
-  addi $sp, $sp, 4
-  
-  jr $ra
+#   // how long is the whole value in register s0? 32-bits.
+
+# Rules for emumeration: 
+#   from right to left, leftmost is least significant.
+#   numeration starts from zero, from right to left. <--
+
+# How to set and clear bits...
+
+addi $t0, $zero, 4  # we create a mask -- bit 2; 
+and $t1, $s0, $t0   # applying the mask to s0 (the original value)
+bne $t1, $t5, L1    # if t1 doesn't contain 0, go to L1
+
+
+
+# better to just use 'or' operation...
+
+
+
+# Create a MIPS program that has a value stored in s0 and set the bits number 4 and 6.
+
+addi $t0, $zero, 0x50     # create a mask. 0x50 also can write, ' addi $t0, $zero, 80 ' (80 = 2^4 + 2^6) in decimal.
+or #s0, $s0, $t0          # apply the mask.
