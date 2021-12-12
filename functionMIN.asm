6eb5b1e3fb7fc1f6315a283e962d861b566f3b82
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-12-12 18:59:25 -0500

    updated registers to s ; preserved by callee

--- functionMIN.asm
+++ functionMIN.asm
@@ -10,8 +10,8 @@ jal MIN                 # jump to function MIN.
 # Our function begins: (jal is not needed when creating a function, only jr)
 
 MIN:                    # parameters in reigster a0 + a1 ; return smallest value to v0. $a are aurgment registers.
-  slt $t0, $a0, $a1     # t0 = is a0 < a1 ? // is a0 smaller than a1?
-  bne $t0, $zero, NOT   # if a0 < a1 is true (1) --> go to NOT label
+  slt $s0, $a0, $a1     # s0 = is a0 < a1 ? // is a0 smaller than a1?
+  bne $s0, $zero, NOT   # if a0 < a1 is true (1) --> go to NOT label
   add $v0, $zero, $a1   # if false (0), store the value of a1 into v0; a1 holds the smallest value.
   jr $ra                # return to program code. exit function.
    
