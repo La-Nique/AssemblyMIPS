c3e6a251fbe9678bc41e52748f09cbb2e5b530e1
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-12-12 22:21:18 -0500

    updated comments & code. complied on MARS

--- countCaps.asm
+++ countCaps.asm
@@ -30,34 +30,36 @@
 COUNT_CAPS:
 	addi $v1, $zero, 0
 	# v1 = captial letter counter 
+	addi $s5, $zero, 64
+	# char 'A' ascii
 
 	NOT_EMPTY:
-        lb $a0, 0($a3) 
-        # a0 = first char
-        addi $a3, $a3, 1
-        # index increment
-        beq $a0, $zero, EXIT
-        # if string is empty
-        bne $a0, $zero, IF_CAPITAL
+		lb $a0, 0($a3) 
+		# a0 = current letter of our input string
+		addi $a3, $a3, 1
+		# index increment
+		beq $a0, $zero, EXIT
+		# if string is at the end --> exit.
+		bne $a0, $zero, IF_CAPITAL
+		# if not --> if capital label.
 
-		IF_CAPITAL:
-          		slti $s7, $a0, 91
-      			# $a3 < 91 ?
-      
-      			addi $s5, $zero, 64
-      			slt $s6, $s5, $a0
-      			# is $s5 (64) < a3 ?
-      
-      		beq $s6, $s7, COUNT
-      		# if 1 = 1 --> Label
-      		addi $s0, $s0, 1
-        	# counter++;
-      	
-      		j NOT_EMPTY
+	IF_CAPITAL:
+		slti $s7, $a0, 91
+		# $a3 < 91 ('Z')?
+		
+		slt $s6, $s5, $a0
+		# is $s5 (64 / 'A') < a3 ?
+
+		beq $s6, $s7, COUNT
+		# if 1 = 1 --> count label.
+
+	j NOT_EMPTY
     
-      COUNT:
-            addi $v1,$v1,1
-            j NOT_EMPTY
+     	COUNT:
+            	addi $v1,$v1,1
+		# captial letter counter++;
+		
+        j NOT_EMPTY
               
   EXIT:
-    jr $ra
+    	jr $ra
