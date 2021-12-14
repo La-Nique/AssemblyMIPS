d3dfc4757b3cfb00098bfa78938e8ff3eb6f4d32
La-Nique <54916166+La-Nique@users.noreply.github.com>
2021-12-14 15:08:22 -0500

    initial commit

---
+++ payment.asm
@@ -0,0 +1,10 @@
+# Write in MIPS a function PAYMENT:
+# PAYMENT takes a number of hours (integer) from $a0 and hourly rate (integer) from $a1. 
+# PAYMENT returns the amount of money earned in $v0.
+
+# Function PAYMENT must use function MYMUL for multiplication. 
+# The function MYMUL takes integers from $a2 and $a3 and returns in $v1 their product. 
+# You don’t have to implement MYMUL, you just have to use it in your code. You have no idea how MYMUL is implemented. 
+# For you, it is just a correct MIPS function that does multiplication as specified.
+
+# Function PAYMENT should follow all MIPS function calling conventions.
